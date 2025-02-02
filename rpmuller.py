de4ffd7d370efc39ae0361d6b3a07055f265d727
Rick Muller <rpmuller@gmail.com>
2025-02-02 09:24:54 -0500

    Got basics working

--- rpmuller.py
+++ rpmuller.py
@@ -1,7 +1,7 @@
 from pystache import render
 
 Name = "Rick Muller"
-PageTitle = f"{Name} Resume"
+PageTitle = Name + " Resume"
 PageSummary = """\
 Director of the Intelligence Advance Research Projects Activity,
 where we develop high-risk, high-payoff capabilities for the US Intelligence
@@ -15,8 +15,6 @@ LinkedInURL = "https://in.linkedin.com/in/rick-muller-62082a"
 GithubURL = "http://github.com/rpmuller"
 TwitterURL = "http://twitter.com/rpmuller"
 
-output_fname = "rpmuller.html"
-template_fname = "template.html"
 
 Jobs = [
     {"Company": "Intelligence Advanced Research Projects Activity (IARPA)",
@@ -62,34 +60,40 @@ Schools = [
 ]
 
 QuantumPapers = [
+    {"Title": "Impact of Incorporation Kinetics on Device Fabrication with Atomic Precision",
+     "Authors": "Xujiao Gao, Erik Nielsen, Andrew Baczewski, Jonathan E. Moussa, John K. Gamble, Andrew G. Salinger, and Richard P. Muller",
+     "Reference": "Phys. Rev. Applied 16, 054037 (2021)",
+     "URL": "https://journals.aps.org/prapplied/abstract/10.1103/PhysRevApplied.16.054037",
+     "Arxiv": "2105.12074",
+    },
     {"Title": "Advanced Electronic Structure Calculations for Nanoelectronics",
      "Authors": "John K. Gamble, Erik Nielsen, Andrew Baczewski, Jonathan E. Moussa, Xujiao Gao, Andrew G. Salinger, and Richard P. Muller",
      "Reference": "Springer Series in Materials Science series Computational Materials, Chemistry, and Biochemistry: From Bold Initiatives to the Last Mile (2020)",
-     "URL": "",
+     "URL": "https://link.springer.com/chapter/10.1007/978-3-030-18778-1_18",
      "Arxiv": "",
     },
     {"Title": "The Promise of Quantum Simulation",
      "Authors": "Richard P. Muller and Robin Blume-Kohout",
      "Reference": "ACS Nano 9, 7738 (2015)",
-     "URL": "",
+     "URL": "https://pubs.acs.org/doi/10.1021/acsnano.5b03650",
      "Arxiv": "1507.06035",
     },
     {"Title": "ASCR Report on Quantum Computing for Science (2015)",
      "Authors": "Alan Aspuru-Guzik, Wim van Dam, Edward Farhi, Frank Gaitan, Travis Humble, Stephen Jordan, Andrew Landahl, Peter Love, Robert Lucas, Richard Muller, John Preskill, Krysta Svore, Nathan Wiebe, and Carl Williams",
      "Reference": "",
-     "URL": "",
+     "URL": "https://science.osti.gov/-/media/ascr/pdf/programdocuments/docs/ASCRQuantumReport-final.pdf",
      "Arxiv": "",
     },
     {"Title": "QCAD Simulation and Optimization of Semiconductor Quantum Dots",
      "Authors": "X. Gao, E. Nielsen, R. P. Muller, R. W. Young, A. G. Salinger, N. C. Bishop, M. Lilly, M. S. Carroll",
      "Reference": "Journal of Applied Physics, 114, 164302 (2013)",
-     "URL": "",
+     "URL": "https://pubs.aip.org/aip/jap/article-abstract/114/16/164302/395457/Quantum-computer-aided-design-simulation-and?redirectedFrom=fulltext",
      "Arxiv": "1403.7561",
     },
     {"Title": "Coherent Electron Transport by Adiabatic Passage in an Imperfect Donor Chain",
      "Authors": "Rajib Rahman, Richard P. Muller, James E. Levy, Malcolm S. Carroll, Gerhard Klimeck, Andrew D. Greentree, Lloyd C. L. Hollenberg",
      "Reference": "Physical Review B, 82, 155315 (2010)",
-     "URL": "",
+     "URL": "https://journals.aps.org/prb/abstract/10.1103/PhysRevB.82.155315",
      "Arxiv": "1008.1494",
     },
 ]
@@ -98,31 +102,31 @@ TheoryPapers = [
     {"Title": "SymPy: Symbolic Computing in Python",
      "Authors": "Aaron Meurer et al.",
      "Reference": "PeerJ Computer Science 3:e103 (2017}",
-     "URL": "",
+     "URL": "https://peerj.com/articles/cs-103/",
      "Arxiv": "1502.05589",
     },
-    {"Title": "U.S. Leadership in High Performance Computing: A Report from the NSA-DOE Technical Meeting on High Performance Computing",
+    {"Title": "U.S. Leadership in High Performance Computing",
      "Authors": "Richard P. Muller and David Mountain",
-     "Reference": "",
-     "URL": "",
+     "Reference": "A Report from the NSA-DOE Technical Meeting on High Performance Computing",
+     "URL": "https://www.nitrd.gov/nitrdgroups/images/b/b4/nsa_doe_hpc_techmeetingreport.pdf",
      "Arxiv": "",
     },
     {"Title": "Optimized Effective Potential from a Correlated Wave Function: OEP-GVB",
      "Authors": "Richard P. Muller and Michael P. Desjarlais",
      "Reference": "Journal of Chemical Physics. 125, 54101 (2006)",
-     "URL": "",
+     "URL": "https://pubs.aip.org/aip/jcp/article-abstract/125/5/054101/908943/Optimized-effective-potential-from-a-correlated?redirectedFrom=fulltext",
      "Arxiv": "",
     },
     {"Title": "Computing Approximate Eigenpairs of Symmetric Block Tridiagonal Matrices",
      "Authors": "Wilfried Gansterer, Robert C. Ward, Richard P. Muller and William A. Goddard, III",
      "Reference": "SIAM Journal on Scientific Computing, 25, 65 (2003)",
-     "URL": "",
+     "URL": "https://epubs.siam.org/doi/10.1137/S1064827501399432",
      "Arxiv": "",
     },
     {"Title": "Ab Initio Calculations of Free Energy Barriers for Chemical Reactions in Solution",
      "Authors": "Richard P. Muller and Arieh Warshel",
      "Reference": "Journal of Physical Chemistry, 99 (49), 17516 (1995)",
-     "URL": "",
+     "URL": "https://pubs.acs.org/doi/10.1021/j100049a009",
      "Arxiv": "",
     },
 ]
@@ -131,28 +135,32 @@ MaterialsPapers = [
     {"Title": "Influence of overcharge and over-discharge on the impedance response of LiCoO2|C batteries",
      "Authors": "Salim Erol, Mark E. Orazem and Richard P. Muller",
      "Reference": "Journal of Power Sources, 270, 92 (2014)",
-     "URL": "",
+     "URL": "https://iopscience.iop.org/article/10.1149/MA2011-02/17/1469",
      "Arxiv": "",
     },
     {"Title": "Simulation of Abuse Behavior of Lithium-Ion Batteries",
      "Authors": "Robert Spotnitz and Richard P. Muller",
      "Reference": "The Electrochemical Society Interface, 21, 54 (2012)",
-     "URL": "",
+     "URL": "https://www.electrochem.org/dl/interface/sum/sum12/sum12_p057_060.pdf",
      "Arxiv": "",
     },
     {"Title": "Meccano on the Nanoscale: A Blueprint for Making the World's Smallest Devices",
      "Authors": "Amar H. Flood, Robert J. A. Ramirez, Wei-Qiao Deng, Richard P. Muller, William A. Goddard, III and J. Frasier Stoddard",
      "Reference": "Australian Journal of Chemistry, 57, 301 (2004)",
-     "URL": "",
+     "URL": "https://www.publish.csiro.au/CH/CH03307",
      "Arxiv": "",
     },
     {"Title": "Quantum Mechanical-Rapid Prototyping Applied to Methane Activation",
      "Authors": "Richard P. Muller, Dean M. Philipp and William A. Goddard, III",
      "Reference": "Topics in Catalysis, 23, 81 (2003)",
-     "URL": "",
+     "URL": "https://link.springer.com/article/10.1023/A:1024872320512",
      "Arxiv": "",
     },
 ]
+
+output_fname = "rpmuller.html"
+template_fname = "template.mustache"
+
 template = open(template_fname).read()
-print(template)
-open(output_fname, "w").write(render(template, locals()))
+output = render(template, locals())
+open(output_fname, "w").write(output)
